b6839567036e0956b0bacdd64aaa27573e8555fb
sxc <qiuyequ@icloud.com>
2021-05-22 07:57:23 +0800

    add modal

--- UltimatePortfolio/DataController.swift
+++ UltimatePortfolio/DataController.swift
@@ -25,4 +25,54 @@ class DataController: ObservableObject {
             
         }
     }
+    
+    
+    func createSampleData() throws {
+        let viewContext = container.viewContext
+        
+        for i in 1...5 {
+            let project = Project(context: viewContext)
+            project.title = "Project \(i)"
+            project.items = []
+            project.creationDate = Date()
+            project.closed = Bool.random()
+            
+            for j in 1...10 {
+                let item = Item(context: viewContext)
+                item.title = "Item \(j)"
+                item.creationDate = Date()
+                item.completed = Bool.random()
+                item.project = project
+                item.priority = Int16.random(in: 1...3)
+            }
+        }
+        try viewContext.save()
+    }
+    
+    func save() {
+        if container.viewContext.hasChanges {
+            try? container.viewContext.save()
+        }
+    }
+    
+    func delete(_ object: NSManagedObject) {
+        container.viewContext.delete(object)
+    }
+    
+    func deleteAll() {
+        let fetchRequest1: NSFetchRequest<NSFetchRequestResult> = Item.fetchRequest()
+        let batchDeleteRequest1 = NSBatchDeleteRequest(fetchRequest: fetchRequest1)
+        _ = try? container.viewContext.execute(batchDeleteRequest1)
+        
+        let fetchRequest2: NSFetchRequest<NSFetchRequestResult> = Project.fetchRequest()
+        let batchDeleteRequest2 = NSBatchDeleteRequest(fetchRequest: fetchRequest2)
+        _ = try? container.viewContext.execute(batchDeleteRequest2)
+        
+    }
+    
+//    static var preview: DataController = {
+//        let dataController = DataController(inMemory: true)
+//        let viewConten
+//    }
+    
 }
