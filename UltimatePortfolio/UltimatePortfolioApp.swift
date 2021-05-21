fa4855fe0bdb17b32a7cb0fda2cef116563ab817
sxc <qiuyequ@icloud.com>
2021-05-21 13:53:22 +0800

    Initial Commit

---
+++ UltimatePortfolio/UltimatePortfolioApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioApp.swift
+//  UltimatePortfolio
+//
+//  Created by Xiaochun Shen on 2021/5/21.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
